a2ebd52d5ca598dd3343d94c1e6294b0b0cc9c03
JS-DEV7923 <sainijatin101.official@gmail.com>
2025-07-08 14:43:00 +0530

    Initial commit

---
+++ vite-tesing/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Samosa from './Samosa.jsx';
+
+function App() {
+  
+
+  return (
+    <>
+    <Samosa />
+    <h1 className='bg-green-400'> chutney .. hari chutney</h1>
+    </>
+  )
+}
+
+export default App
---
+++ vite-tesing/src/Samosa.jsx
@@ -0,0 +1,6 @@
+function Samosa() {
+    return (
+        <h1>chai aur samosa ... best combo ^.^</h1>
+    )
+}
+export default Samosa;
